d3e1ab3ef59afb45c35060ab76f12e1cc7d96925
ew-prog <ewanyama@live.co.uk>
2025-10-18 06:41:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/leads', leadsRoutes)
 
-app.listen(process.env.PORT || 5000, () => console.log('Server running'))
+app.listen(process.env.PORT || 5000, () => console.log('Backend running'))
